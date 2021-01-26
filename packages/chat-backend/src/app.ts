ab22c11888c29235e6a3f626499dac650ef963f7
hyperflow <hyperflow@kakao.com>
2021-01-27 04:59:27 +0900

    project-setup: 미들웨어 추가

--- packages/chat-backend/src/app.ts
+++ packages/chat-backend/src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
 const { NODE_ENV } = process.env;
+
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+export default app;
--- packages/chat-backend/src/index.ts
+++ packages/chat-backend/src/index.ts
@@ -1,1 +1,7 @@
+import server from 'app';
+
 const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server listening: ${port}`);
+});
